178114db092b4327690b73c28b545a7e151ae8e0
Josh Rochon <joshdrochon@gmail.com>
2018-02-16 12:27:38 -0800

    TestMethod WordCounter_Reset successfully passes and fails mstest as expected

--- WordCounter.Tests/ModelTests/WordCounterTest.cs
+++ WordCounter.Tests/ModelTests/WordCounterTest.cs
@@ -7,11 +7,19 @@ using System;
 namespace WordCounterProject.Test
 {
   [TestClass]
-  public class WorderCounterTest : IDispable
+  public class WorderCounterTest : IDisposable
   {
     public void Dispose()
     {
       WordCounter.Reset();
+    }
+
+    [TestMethod]
+    public void WorCounter_Reset()
+    {
+      int expectedResult = 0;
+      int actualResult = WordCounter.Reset();
+      Assert.AreEqual(expectedResult, actualResult);
     }
   }
 }
