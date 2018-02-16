0d87a1fd67e1d3f0b6715b1cbbf8b071c15e5f86
Josh Rochon <joshdrochon@gmail.com>
2018-02-16 12:53:04 -0800

    TestMethod testCalcPass successfully passes test

--- WordCounter.Tests/ModelTests/WordCounterTest.cs
+++ WordCounter.Tests/ModelTests/WordCounterTest.cs
@@ -1,8 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using WordCounterProject.Models;
 using WordCounterProject;
-using System.Collections.Generic;
-using System;
 
 namespace WordCounterProject.Test
 {
@@ -15,11 +15,27 @@ namespace WordCounterProject.Test
     }
 
     [TestMethod]
-    public void WorCounter_Reset()
+    public void WordCounter_Reset()
     {
+      //arrange
       int expectedResult = 0;
+      //act
       int actualResult = WordCounter.Reset();
+      //assert
       Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [TestMethod]
+    public void TestCalc_Pass()
+    {
+      //arrange
+      int expectedCounterVal = 1;
+      WordCounter testCalcPass = new WordCounter();
+      //act
+      int actualCounterVal = testCalcPass.WordCounterCalc
+      ("random", "This is a random sentence.");
+      //assert
+      Assert.AreEqual(expectedCounterVal, actualCounterVal);
     }
   }
 }
